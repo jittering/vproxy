cd768a8144fb76439dbf4f8c4be3a3efed613572
Chetan Sarva <chetan@pixelcop.net>
2020-09-29 09:18:21 -0400

    refactor: break down main method

--- bin/vproxy/main.go
+++ bin/vproxy/main.go
@@ -17,7 +17,7 @@ import (
 
 var (
 	bind      = flag.String("bind", "", "Bind hostnames to local ports (app.local.com:7000)")
-	httpPort  = flag.Int("http", 80, "Port to listen for HTTP on")
+	httpPort  = flag.Int("http", 80, "Port to listen for HTTP")
 	httpsPort = flag.Int("https", 443, "Port to listen for TLS (HTTPS)")
 	useTLS    = flag.Bool("tls", false, "Enable TLS")
 )
@@ -41,6 +41,8 @@ func main() {
 	mux := simpleproxy.NewLoggedMux()
 	mux.Handle("/", vhost)
 
+	// start listeners
+
 	var wg sync.WaitGroup
 	wg.Add(1)
 
@@ -54,32 +56,40 @@ func main() {
 	if *useTLS {
 		wg.Add(1)
 		go func() {
-			listenAddrTLS := fmt.Sprintf("127.0.0.1:%d", 443)
-			fmt.Printf("[*] starting proxy: https://%s\n", listenAddrTLS)
-			fmt.Printf("    vhosts:\n")
-			cfg := &tls.Config{}
-			for _, server := range vhost.Servers {
-				fmt.Printf("    - %s -> %d\n", server.Host, server.Port)
-				cert, err := tls.LoadX509KeyPair(server.Cert, server.Key)
-				if err != nil {
-					log.Fatal("failed to load keypair:", err)
-				}
-				cfg.Certificates = append(cfg.Certificates, cert)
-			}
-			cfg.BuildNameToCertificate()
-
-			server := http.Server{
-				Addr:      listenAddrTLS,
-				Handler:   mux,
-				TLSConfig: cfg,
-			}
-			server.ListenAndServeTLS("", "")
-
+			listenTLS(vhost, mux)
 			wg.Done()
 		}()
 	}
 
 	wg.Wait()
+}
+
+func listenTLS(vhost *simpleproxy.VhostMux, mux *simpleproxy.LoggedMux) {
+	listenAddrTLS := fmt.Sprintf("127.0.0.1:%d", 443)
+	fmt.Printf("[*] starting proxy: https://%s\n", listenAddrTLS)
+	fmt.Printf("    vhosts:\n")
+
+	server := http.Server{
+		Addr:      listenAddrTLS,
+		Handler:   mux,
+		TLSConfig: createTLSConfig(vhost),
+	}
+	server.ListenAndServeTLS("", "")
+}
+
+// Create multi-certificate TLS config from vhost config
+func createTLSConfig(vhost *simpleproxy.VhostMux) *tls.Config {
+	cfg := &tls.Config{}
+	for _, server := range vhost.Servers {
+		fmt.Printf("    - %s -> %d\n", server.Host, server.Port)
+		cert, err := tls.LoadX509KeyPair(server.Cert, server.Key)
+		if err != nil {
+			log.Fatal("failed to load keypair:", err)
+		}
+		cfg.Certificates = append(cfg.Certificates, cert)
+	}
+	cfg.BuildNameToCertificate()
+	return cfg
 }
 
 // Create vhost config for each binding
