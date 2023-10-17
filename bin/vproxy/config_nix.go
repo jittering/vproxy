78f76fee8a68a4b20079dc486cc9e14b4ef0e7e3
Chetan Sarva <chetan@pixelcop.net>
2023-10-17 10:23:52 -0400

    feat: added new homebrew path

--- bin/vproxy/config_nix.go
+++ bin/vproxy/config_nix.go
@@ -8,6 +8,13 @@ func findConfigFile(path string, isDaemon bool) string {
 		// look for dot file only for clients
 		paths = append(paths, ".vproxy.conf")
 	}
-	paths = append(paths, homeConfPath(), "/usr/local/etc/vproxy.conf", "/etc/vproxy.conf")
+
+	paths = append(paths,
+		homeConfPath(),
+		"/usr/local/etc/vproxy.conf",
+		"/opt/homebrew/etc/vproxy.conf",
+		"/etc/vproxy.conf",
+	)
+
 	return findConfig(paths...)
 }
